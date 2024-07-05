20499081fcf0022f789bc41913e2e329a985ef42
Daniel Ceriana <danielcerianax@gmail.com>
2024-07-05 14:09:53 -0300

    primer commit

---
+++ carrera_utn.py
@@ -0,0 +1,254 @@
+import pygame
+import colores
+from datos import lista
+from funciones import *
+pygame.init()
+ANCHO_VENTANA = 1280
+ALTO_VENTANA = 720
+
+#crear una fuente
+dict_fuentes={
+    "fuente" : pygame.font.SysFont("Arial",30),
+    "fuente_respuestas" : pygame.font.SysFont("Arial",20),
+    "fuente_boost" : pygame.font.SysFont("Arial",10)
+}
+
+
+#assets
+#cargar logo
+
+imagen = pygame.image.load("logo_carrera_de_mente.png")
+imagen = pygame.transform.scale(imagen,(200,200))
+imagen_utn = pygame.image.load("Logo-utn.png")
+imagen_utn = pygame.transform.scale(imagen_utn,(90,45))
+    #personaje
+personaje = pygame.image.load("personaje.png")
+personaje = pygame.transform.scale(personaje,(100,100))
+
+dict_assets = {
+    "imagen":imagen,
+    "imagen_utn":imagen_utn,
+    "personaje":personaje
+}
+
+#props estado_de_juego
+    
+estado_de_juego = { 
+    "pregunta" : "",
+    "tema" : "",
+    "score" : 0,
+    "respuesta_a" : "",
+    "respuesta_b" : "",
+    "respuesta_c" : "",
+    "contador" : 0,
+    "respuesta_apretada" : "",
+    "flag_siguiente_pregunta" : False,
+    "flag_primera_ejecucion" : True,
+    "respuestas_visibles" : False,
+    "pos_personaje" : [0,0],
+    "indice_pos_personaje" : 0,
+    "segundos" : "5",
+    "fin_tiempo" : False,
+    "nombre_jugador" : "",
+    "mostrar_scores":False,
+    "guardado":False,
+    "vista" : "juego",
+    "lista_preguntas" : [],
+    "lista_temas" : [],
+    "lista_respuesta_a" : [],
+    "lista_respuesta_b" : [],
+    "lista_respuesta_c" : [],
+    "lista_correctas" : []
+}
+
+for e_lista in lista:
+    estado_de_juego["lista_preguntas"].append(e_lista["pregunta"])
+    estado_de_juego["lista_temas"].append(e_lista["tema"])
+    estado_de_juego["lista_respuesta_a"].append(e_lista["a"])
+    estado_de_juego["lista_respuesta_b"].append(e_lista["b"])
+    estado_de_juego["lista_respuesta_c"].append(e_lista["c"])
+    estado_de_juego["lista_correctas"].append(e_lista["correcta"])
+
+#casillas
+DIFERENCIA = 80
+DIFERENCIA_ALTO = 100
+ANCHO_CASILLA = 75
+ALTO_CASILLA = 75
+INICIO = (300,340,75,75)
+FIN = (INICIO[0],INICIO[1]+DIFERENCIA_ALTO,75,75)
+#9x2
+lista_casillas = [INICIO,(INICIO[0]+DIFERENCIA,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                    (INICIO[0]+DIFERENCIA*2,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*3,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*4,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*5,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*6,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*7,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*8,INICIO[1],ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*8,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*7,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*6,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*5,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*4,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*3,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA*2,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  (INICIO[0]+DIFERENCIA,INICIO[1] + DIFERENCIA_ALTO ,ANCHO_CASILLA,ALTO_CASILLA),
+                  FIN]
+casillas_especiales = [{"numero":6,"boost":1},
+                       {"numero":13,"boost":-1}]
+
+#posiciones botones
+    
+# pos_siguiente_pregunta = (300,20,300,100)
+ANCHO_RESPUESTA = ANCHO_VENTANA / 6
+ALTO_RESPUESTA = 100
+POS_CUADRO_RESPUESTAS = (300,30,950,250)
+pos_comenzar = (ANCHO_VENTANA*0.5-300,570,220,100)
+pos_terminar = (ANCHO_VENTANA*0.5+100,570,200,100)
+pos_respuesta_A = (ANCHO_RESPUESTA*2,ALTO_RESPUESTA,ANCHO_RESPUESTA-10,100)
+pos_respuesta_B = (ANCHO_RESPUESTA*3,ALTO_RESPUESTA,ANCHO_RESPUESTA-10,100)
+pos_respuesta_C = (ANCHO_RESPUESTA*4,ALTO_RESPUESTA,ANCHO_RESPUESTA-10,100)
+
+
+#posiciones de variables
+pos_score = (ANCHO_RESPUESTA*5+20,100)
+pos_score_variable = (ANCHO_RESPUESTA*5+20,150)
+pos_tiempo = (ANCHO_RESPUESTA*5+20,200)
+pos_tiempo_variable = (ANCHO_RESPUESTA*5+20,250)
+pos_tema = (ANCHO_RESPUESTA*2,220)
+pos_pregunta = (ANCHO_RESPUESTA*2,50)
+pos_avanza = casillas_especiales[0]["numero"]
+pos_retrocede = casillas_especiales[1]["numero"]
+
+dict_textos={
+    "TEXTO_SCORE" :{"texto": dict_fuentes["fuente"].render(str("SCORE"),True,colores.BLACK),"pos":pos_score},
+    "TEXTO_COMENZAR" :{"texto": dict_fuentes["fuente"].render(str("COMENZAR"),True,colores.BLACK),"pos":(pos_comenzar[0]+20,
+                                                                                        pos_comenzar[1]+30)},
+    "TEXTO_TERMINAR" :{"texto": dict_fuentes["fuente"].render(str("TERMINAR"),True,colores.BLACK),"pos":(pos_terminar[0]+20,
+                                                                                        600)},
+    "TEXTO_TIEMPO" :{"texto": dict_fuentes["fuente"].render(str("TIEMPO"),True,colores.BLACK),"pos":pos_tiempo},
+    "TEXTO_AVANZA" :{"texto": dict_fuentes["fuente_boost"].render(str("Avanza 1"),True,colores.BLACK),"pos":pos_avanza},
+    "TEXTO_RETROCEDE" :{"texto": dict_fuentes["fuente_boost"].render(str("Retrocede 1"),True,colores.BLACK),"pos":pos_retrocede},
+    "texto_tema":{"texto" :  dict_fuentes["fuente"].render(str("tema"),True,colores.COLOR_AMARILLO),"pos":pos_tema},
+    "texto_pregunta":{"texto" :  dict_fuentes["fuente"].render(str("pregunta"),True,colores.BLACK),"pos":pos_pregunta},
+    "texto_score_variable":{"texto" : dict_fuentes["fuente"].render(str(estado_de_juego["score"]),True,colores.BLACK),"pos":pos_score_variable},
+    "texto_tiempo_variable":{"texto" :  dict_fuentes["fuente"].render(str(estado_de_juego["segundos"]),True,colores.BLACK),"pos":pos_tiempo_variable},
+    "texto_respuesta_a":{"texto" : dict_fuentes["fuente_respuestas"].render(str("A"),True,colores.COLOR_AMARILLO),"pos":pos_respuesta_A},
+    "texto_respuesta_b":{"texto" : dict_fuentes["fuente_respuestas"].render(str("B"),True,colores.COLOR_AMARILLO),"pos":pos_respuesta_B},
+    "texto_respuesta_c":{"texto" : dict_fuentes["fuente_respuestas"].render(str("C"),True,colores.COLOR_AMARILLO),"pos":pos_respuesta_C},
+    "CUADRO_RESPUESTAS":{"texto":"","pos":POS_CUADRO_RESPUESTAS},
+    "CUADRO_COMENZAR":{"texto":"","pos":pos_comenzar},
+    "CUADRO_TERMINAR":{"texto":"","pos":pos_terminar},
+    "CUADRO_RESPUESTA_A":{"texto":"","pos":pos_respuesta_A},
+    "CUADRO_RESPUESTA_B":{"texto":"","pos":pos_respuesta_B},
+    "CUADRO_RESPUESTA_C":{"texto":"","pos":pos_respuesta_C}
+}
+    
+
+#timer
+timer_segundos = pygame.USEREVENT
+pygame.time.set_timer(timer_segundos,1000)
+
+#crear la pantalla
+pantalla = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
+pygame.display.set_caption("Carrera UTN")
+
+flag_correr = True
+while flag_correr:
+    lista_eventos = pygame.event.get()
+    for evento in lista_eventos:
+        if evento.type == pygame.QUIT:
+            flag_correr = False
+        if evento.type == pygame.USEREVENT:
+            if evento.type == timer_segundos:
+                if not estado_de_juego["flag_primera_ejecucion"]:
+                    if estado_de_juego["fin_tiempo"] == False:
+                        estado_de_juego["segundos"] = int(estado_de_juego["segundos"]) - 1
+                    if int(estado_de_juego["segundos"]) == 0:
+                        estado_de_juego["flag_siguiente_pregunta"] = True
+        if evento.type == pygame.KEYDOWN:
+            if estado_de_juego["fin_tiempo"]  == True and estado_de_juego["vista"] == "nombre":
+                if evento.key == pygame.K_BACKSPACE:
+                    estado_de_juego["nombre_jugador"] = estado_de_juego["nombre_jugador"][0:-1]
+                    print(estado_de_juego["nombre_jugador"])
+                elif evento.key == pygame.K_RETURN:
+                    estado_de_juego["mostrar_scores"] = True
+                elif estado_de_juego["fin_tiempo"] == True:
+                    estado_de_juego["nombre_jugador"] += evento.unicode 
+                    print(estado_de_juego["nombre_jugador"])
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            
+            if chequear_click_en_rect(evento.pos,pos_respuesta_A):
+                if estado_de_juego["respuestas_visibles"]: 
+                    cambiar_respuesta_apretada(estado_de_juego,"respuesta_apretada",
+                                               'a',"flag_siguiente_pregunta")
+            elif chequear_click_en_rect(evento.pos,pos_respuesta_B):
+                if estado_de_juego["respuestas_visibles"]:
+                    cambiar_respuesta_apretada(estado_de_juego,"respuesta_apretada",
+                                               'b',"flag_siguiente_pregunta")
+            elif chequear_click_en_rect(evento.pos,pos_respuesta_C):
+                if estado_de_juego["respuestas_visibles"]:
+                    cambiar_respuesta_apretada(estado_de_juego,"respuesta_apretada",
+                                               'c',"flag_siguiente_pregunta")
+            if estado_de_juego["lista_correctas"][estado_de_juego["contador"]] == estado_de_juego["respuesta_apretada"]:
+                estado_de_juego["score"] += 10
+                if(estado_de_juego["indice_pos_personaje"] + 2 >= len(lista_casillas)):
+                    #fin juego
+                    estado_de_juego["indice_pos_personaje"] = len(lista_casillas) - 1
+                    frenar_juego(estado_de_juego)
+                    print("GANASTE")
+                elif verificar_casilla_especial(estado_de_juego,casillas_especiales) != 0 : 
+                    # print("pasa por una casilla especial")
+                    estado_de_juego["indice_pos_personaje"] += 2
+                else:
+                    estado_de_juego["indice_pos_personaje"] += 2
+            elif estado_de_juego["respuesta_apretada"] != '' :
+                if estado_de_juego["indice_pos_personaje"] - 1 <= 0:
+                    frenar_juego(estado_de_juego)
+                else:
+                    estado_de_juego["indice_pos_personaje"] -= 1
+            if chequear_click_en_rect(evento.pos,pos_terminar):
+                frenar_juego(estado_de_juego)
+            if chequear_click_en_rect(evento.pos,pos_comenzar):
+                reiniciar_estado_juego(estado_de_juego)
+                if estado_de_juego["flag_primera_ejecucion"]:
+                    estado_de_juego["flag_primera_ejecucion"] = False
+                estado_de_juego["respuestas_visibles"] = True
+            
+                
+        estado_de_juego["respuesta_apretada"] = ""            
+    pantalla.fill(colores.BLUE)
+    
+    ##control siguiente pregunta
+    if estado_de_juego["flag_siguiente_pregunta"]:
+        if estado_de_juego["contador"] == len(estado_de_juego["lista_preguntas"])-1:
+                frenar_juego(estado_de_juego)
+                # terminar_juego(estado_de_juego,pygame,pantalla,colores)
+
+        else:
+            estado_de_juego["contador"] += 1
+        estado_de_juego["respuesta_apretada"] = "" 
+        estado_de_juego["flag_siguiente_pregunta"] = False
+        estado_de_juego["segundos"] = "5"
+
+    #movimiento personaje
+    boost = verificar_casilla_especial(estado_de_juego,casillas_especiales) 
+    estado_de_juego["indice_pos_personaje"] += boost
+    estado_de_juego["pos_personaje"] = list(lista_casillas[estado_de_juego["indice_pos_personaje"]])     
+
+
+# vistas = [{"nombre":"juego"},{"nombre":"nombre"},{"nombre":"puntajes"}]
+    match estado_de_juego["vista"]:
+        case "juego":
+            mostrar_juego(estado_de_juego,pygame,pantalla,colores,dict_textos,
+                          lista_casillas,dict_fuentes,dict_assets)
+        case "nombre":
+            mostrar_nombre(estado_de_juego,pygame,pantalla,colores)            
+        case "puntajes":
+            puntajes = cargar_puntajes()
+            if len(puntajes) > 0:
+                mostrar_puntajes(puntajes,pygame,pantalla,colores) 
+    pygame.display.flip()
+    
+pygame.quit()
+
---
+++ funciones.py
@@ -0,0 +1,283 @@
+from funciones_archivos import *
+from copy import deepcopy
+def chequear_click_en_rect(posicion_click:tuple,item_rect:tuple)->bool:
+     '''
+     chequea que el click haya sido en un area determinada
+     Parametros: posicion_click:tuple
+                    item_rect:tuple
+     Retorno: bool => True si cliquea en el rect, False si no
+     '''
+     if ((posicion_click[0] > item_rect[0] and 
+          posicion_click[0] < item_rect[0]+item_rect[2])
+          and (posicion_click[1] > item_rect[1] and 
+               posicion_click[1] < item_rect[1] + item_rect[3])):
+                    return True
+     return False
+
+def cambiar_respuesta_apretada(estado_juego:dict,key_respuesta_apretada:str,
+                               respuesta:str,key_flag_siguiente_pregunta:str):
+     estado_juego[key_respuesta_apretada] = respuesta
+     estado_juego[key_flag_siguiente_pregunta] = True
+     
+def reiniciar_estado_juego(estado_juego:dict):
+     '''
+     funcion especifica a carrera_utn para reiniciar el estado de juego
+     Parametros: estado_juego:dict => variables de entorno de juego
+     Retorno: No
+     '''
+     estado_de_juego_por_defecto = { 
+          "respuestas_visibles" : True,
+          "contador" : 0,
+          "flag_siguiente_pregunta" : False,
+          "respuesta_apretada" : "",
+          "score" : 0,
+          "segundos" : '5',
+          "indice_pos_personaje" : 0,
+          "fin_tiempo" : False,
+          "vista" : "juego"
+}
+     for key in estado_de_juego_por_defecto.keys():
+          estado_juego[key] = estado_de_juego_por_defecto[key]
+
+     
+def actualizar_textos(estado_juego:dict,listas:dict,fuente,fuente_respuestas,colores,textos):
+     '''
+     funcion especifica a carrera_utn para actualizar los textos
+     Parametros: estado_juego:dict => variables de entorno de juego
+               listas:dict => 
+               fuente => fuente para los render
+               fuente_respuestas => fuente para los render de respuesta
+               colores => constantes colores
+               textos => textos a renderizar
+     Retorno: No    
+     '''
+     estado_juego["pregunta"] = listas["lista_preguntas"][estado_juego["contador"]]
+     estado_juego["tema"] = listas["lista_temas"][estado_juego["contador"]]
+     estado_juego["respuesta_a"] = listas["lista_respuesta_a"][estado_juego["contador"]]
+     estado_juego["respuesta_b"] = listas["lista_respuesta_b"][estado_juego["contador"]]
+     estado_juego["respuesta_c"] = listas["lista_respuesta_c"][estado_juego["contador"]]
+     textos["texto_tema"]["texto"] = fuente.render(str(estado_juego["tema"]),True,colores.COLOR_AMARILLO)
+     textos["texto_pregunta"]["texto"] = fuente.render(str(estado_juego["pregunta"]),True,colores.BLACK)
+     textos["texto_respuesta_a"]["texto"] = fuente_respuestas.render(str(estado_juego["respuesta_a"]),True,colores.COLOR_AMARILLO)
+     textos["texto_respuesta_b"]["texto"] = fuente_respuestas.render(str(estado_juego["respuesta_b"]),True,colores.COLOR_AMARILLO)
+     textos["texto_respuesta_c"]["texto"] = fuente_respuestas.render(str(estado_juego["respuesta_c"]),True,colores.COLOR_AMARILLO)
+     textos["texto_score_variable"]["texto"] = fuente.render(str(estado_juego["score"]),True,colores.BLACK)
+     textos["texto_tiempo_variable"]["texto"] = fuente.render(str(estado_juego["segundos"]),True,colores.BLACK)
+     
+def frenar_juego(estado_juego:dict):
+     '''
+     funcion especifica a carrera_utn para frenar el estado de juego
+     Parametros: estado_juego:dict => variables de entorno de juego
+     Retorno: No
+     '''
+     estado_juego["respuestas_visibles"] = False
+     # estado_juego["contador"] = 0
+     estado_juego["flag_siguiente_pregunta"] = False
+     estado_juego["respuesta_apretada"] = ""
+     # estado_juego["score"] = 0
+     estado_juego["segundos"] = '5'
+     # estado_juego["indice_pos_personaje"] = 0
+     estado_juego["fin_tiempo"] = True
+     estado_juego["vista"] = "nombre"
+     
+     
+def cargar_puntajes()->list:
+     '''
+     Carga los puntajes del archivo scores.json
+     retorno: ->list puntajes o lista vacia si hubo error
+     '''
+     try:
+          puntajes = leer_json_lista("scores.json")
+          return puntajes
+     except:
+          print('Ocurrio un error al leer el archivo de scores')
+          return []
+     
+def guardar_puntajes(estado_juego):
+     '''
+     guarda los puntajes ordenados en el archivo scores.json
+     
+     '''
+     puntajes = cargar_puntajes()
+     aux_guardar_ordenado = []
+     if not estado_juego["guardado"]:
+          try:
+               if len(puntajes) > 0:
+                    aux_puntajes = deepcopy(puntajes)
+                    if len(estado_juego["nombre_jugador"])>0:
+                         aux_puntajes.append({"nombre":estado_juego["nombre_jugador"],
+                                             "score":estado_juego["score"]})
+                    else:
+                         aux_puntajes.append({"nombre":"NOMBRE_DESCONOCIDO",
+                         "score":estado_juego["score"]})
+                    aux_puntajes.sort(key=lambda item : int(item["score"]), reverse=True)
+                    i = 0
+                    for item in aux_puntajes:
+                         if i < 10:
+                              aux_guardar_ordenado.append(item)
+                         i += 1
+                    guardar_json_lista("scores.json",aux_guardar_ordenado)
+                    estado_juego["guardado"] = True
+                    print("Se guardo el puntaje")
+          except:
+               print('Ocurrio un error al guardar el archivo de scores')
+     else:
+          estado_juego["vista"] = "puntajes"
+          print("cambio de vista a puntaje")
+
+     
+def mostrar_puntajes(lista_puntajes:list,pygame,pantalla,colores):
+     '''
+     Mostrar los puntajes en pantalla
+     Parametros:  lista_puntajes => lista con todos los puntajes
+               pygame => objeto pygame
+               pantalla => lugar donde pintar
+               colores => constantes colores
+     Retorno:
+     '''
+     alto_base_puntajes = 200
+     POS_PUNTAJES = [400,100,100,100]
+     pos_puntajes = [800,alto_base_puntajes,100,100]
+     POS_NOMBRES = [400,alto_base_puntajes,100,100]
+     
+     
+     fuente = pygame.font.SysFont("Arial",30)
+     
+     PUNTAJES = fuente.render(str("PUNTAJES"),True,colores.BLACK)
+     pantalla.blit(PUNTAJES,POS_PUNTAJES)
+
+     
+     if len(lista_puntajes)>0:
+          for item in lista_puntajes:
+               nombre = fuente.render(str(item["nombre"]),True,colores.BLACK)
+               score = fuente.render(str(item["score"]),True,colores.BLACK)
+               pantalla.blit(nombre,POS_NOMBRES)
+               pantalla.blit(score,pos_puntajes)
+               pos_puntajes[1] += 50
+               POS_NOMBRES[1] += 50
+               
+def mostrar_nombre(estado_juego:dict,pygame,pantalla,colores):
+     '''
+     termina el juego y guarda el score
+     Parametros: estado_juego:dict => variables de entorno de juego
+               pygame => objeto pygame
+               pantalla => lugar donde pintar
+               colores => constantes colores
+
+     Retorno: No
+     '''     
+     fuente = pygame.font.SysFont("Arial",30)
+     TEXTO_FINALIZADO = fuente.render(str("JUEGO FINALIZADO, SU PUNTAJE:"),True,colores.BLACK)
+     PUNTAJE_FINALIZADO = fuente.render(str(estado_juego["score"]),True,colores.BLACK)
+     TEXTO_INGRESE_NOMBRE = fuente.render(str("Ingrese su nombre:"),True,colores.BLACK)
+     TEXTO_NOMBRE = fuente.render(str(estado_juego["nombre_jugador"]),True,colores.BLACK)
+     
+
+     POS_FINALIZADO = (100,100,100,100)
+     POS_PUNTAJE_FINALIZADO = (800,100)
+     POS_INGRESE_NOMBRE = (500,400)
+     POS_INGRESE_NOMBRE_VARIABLE = (550,500)
+     if not estado_juego["mostrar_scores"]:
+          # pos_texto_finalizado
+          pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,(
+               POS_INGRESE_NOMBRE_VARIABLE[0]-75,
+               POS_INGRESE_NOMBRE_VARIABLE[1]-30,
+               300,100),5)
+          pantalla.blit(TEXTO_FINALIZADO,POS_FINALIZADO)
+          pantalla.blit(PUNTAJE_FINALIZADO,POS_PUNTAJE_FINALIZADO)
+          pantalla.blit(TEXTO_INGRESE_NOMBRE,POS_INGRESE_NOMBRE)
+          pantalla.blit(TEXTO_NOMBRE,POS_INGRESE_NOMBRE_VARIABLE)
+     else:
+          guardar_puntajes(estado_juego)
+                 
+               
+
+     
+     
+     
+def verificar_casilla_especial(estado_juego:list,lista_casillas_especiales:list):
+     '''
+     devuelve el boost, si es 0 es porque no tiene
+     '''
+     boost = 0
+     pos_personaje = estado_juego["indice_pos_personaje"]
+     for item in lista_casillas_especiales:
+          if pos_personaje == item["numero"]:
+               boost = item["boost"]
+     return boost
+
+def mostrar_textos(pantalla,textos):
+    for key in textos.keys():
+        pantalla.blit(textos[key]["texto"],textos[key]["pos"])
+        
+        
+        
+        
+    
+def mostrar_juego(estado_de_juego,pygame,pantalla,colores,
+                  dict_textos,lista_casillas,dict_fuentes,dict_assets):
+             #realizar todos los cambios de texto
+     if not estado_de_juego["flag_primera_ejecucion"]:
+          actualizar_textos(estado_de_juego,{
+                              "lista_preguntas":estado_de_juego["lista_preguntas"],
+                                "lista_temas":estado_de_juego["lista_temas"],
+                                "lista_respuesta_a":estado_de_juego["lista_respuesta_a"],
+                                "lista_respuesta_b":estado_de_juego["lista_respuesta_b"],
+                                "lista_respuesta_c":estado_de_juego["lista_respuesta_c"]},
+                          dict_fuentes["fuente"],dict_fuentes["fuente_respuestas"],colores,dict_textos)
+        
+     
+
+     pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,dict_textos["CUADRO_COMENZAR"]["pos"])
+     pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,dict_textos["CUADRO_TERMINAR"]["pos"])
+     pygame.draw.rect(pantalla,colores.SKYBLUE4,dict_textos["CUADRO_RESPUESTAS"]["pos"])
+     
+
+     ###casillas
+     lista_colores = [colores.ORANGE,colores.GREEN,colores.SKYBLUE4,
+                    colores.RED1,colores.VIOLET,colores.ORANGE,
+                    colores.YELLOW1,colores.GREEN]
+     indice = 0
+     for pos in lista_casillas:
+          pygame.draw.rect(pantalla,lista_colores[indice],pos)
+          indice += 1
+          if indice >= len(lista_colores):
+               indice = 0
+          
+          
+     #Fundir textos
+     pantalla.blit(dict_textos["TEXTO_SCORE"]["texto"],dict_textos["TEXTO_SCORE"]["pos"])
+     pantalla.blit(dict_textos["texto_score_variable"]["texto"],
+                                   dict_textos["texto_score_variable"]["pos"])
+     pantalla.blit(dict_textos["texto_tema"]["texto"],dict_textos["texto_tema"]["pos"])
+     pantalla.blit(dict_textos["texto_pregunta"]["texto"],dict_textos["texto_pregunta"]["pos"])
+     pantalla.blit(dict_textos["TEXTO_TIEMPO"]["texto"],dict_textos["TEXTO_TIEMPO"]["pos"])
+     pantalla.blit(dict_textos["texto_tiempo_variable"]["texto"],dict_textos["texto_tiempo_variable"]["pos"])
+     
+     #         pantalla.blit(TEXTO_AVANZA,lista_casillas[casillas_especiales[0]["numero"]])
+     #    pantalla.blit(TEXTO_RETROCEDE,lista_casillas[casillas_especiales[1]["numero"]])
+     pantalla.blit(dict_textos["TEXTO_AVANZA"]["texto"],lista_casillas[dict_textos["TEXTO_AVANZA"]["pos"]])
+     pantalla.blit(dict_textos["TEXTO_RETROCEDE"]["texto"],lista_casillas[dict_textos["TEXTO_RETROCEDE"]["pos"]])
+          #preguntas/respuestas
+
+          #botones
+     pantalla.blit(dict_textos["TEXTO_COMENZAR"]["texto"],
+                   dict_textos["TEXTO_COMENZAR"]["pos"])
+     pantalla.blit(dict_textos["TEXTO_TERMINAR"]["texto"],
+                   dict_textos["TEXTO_TERMINAR"]["pos"])
+               #respuestas
+     if estado_de_juego["respuestas_visibles"]:
+          pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,dict_textos["CUADRO_RESPUESTA_A"]["pos"],5)
+          pantalla.blit(dict_textos["texto_respuesta_a"]["texto"],(dict_textos["texto_respuesta_a"]["pos"][0]+15
+                                   ,dict_textos["texto_respuesta_a"]["pos"][1]+35))
+          pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,dict_textos["CUADRO_RESPUESTA_B"]["pos"],5)
+          pantalla.blit(dict_textos["texto_respuesta_b"]["texto"],(dict_textos["texto_respuesta_b"]["pos"][0]+15
+                                   ,dict_textos["texto_respuesta_b"]["pos"][1]+35))
+          pygame.draw.rect(pantalla,colores.COLOR_AMARILLO,dict_textos["CUADRO_RESPUESTA_C"]["pos"],5)  
+          pantalla.blit(dict_textos["texto_respuesta_c"]["texto"],(dict_textos["texto_respuesta_c"]["pos"][0]+15
+                                   ,dict_textos["texto_respuesta_c"]["pos"][1]+35))
+     
+     pantalla.blit(dict_assets["imagen"],(10,10))
+     pantalla.blit(dict_assets["imagen_utn"],(64,100))
+     
+     pantalla.blit(dict_assets["personaje"],estado_de_juego["pos_personaje"])
